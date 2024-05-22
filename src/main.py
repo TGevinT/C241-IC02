fc74eeb1d51caa3be2bf1c36d31b176b49dc26ed
Achmad Noval Fahrezi <achmadnovalfahrezi@Laptop-Achmad.local>
2024-05-22 16:30:40 +0700

    update .gitignore file

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 __version__ = "1.0.0"
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 async def index():
     return {
