d63209e6e01ccce658b70b820213a1bff0fe2ead
TGevinT <t.gevintaufan@gmail.com>
2024-06-04 19:49:31 +0700

    feat: add to bucket, and link in bucket save to database

--- src/gigi_tampak_atas.py
+++ src/gigi_tampak_atas.py
@@ -8,12 +8,14 @@ import io
 current_dir = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(current_dir, 'model', 'model_gigi_tampak_atas.onnx')
 
-
-
 class GigiTampakAtas(Gigi):
     def __init__(self, image):
+        self._image_read = image
         img_convert = Image.open(io.BytesIO(image)).convert('RGB')
         processed_image = preprocess_image_atas(img_convert)
         super().__init__(img_convert, processed_image)
         self._class_labels = ['Bengkak Gusi', 'Bukan Gambar Gigi', 'Gigi Berlubang', 'Gigi Sehat', 'Plak Gigi']
         self._model = ort.InferenceSession(model_path)
+        self._bucket_name = "gigi-tampak-atas"
+
+
